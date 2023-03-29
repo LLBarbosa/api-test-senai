f7d9f5f19842dd28ddd44fb35f6049312f9f0db1
douglas-cavalcante <douglascavalcante2803@gmail.com.br>
2023-03-28 22:03:54 -0300

    feat: adiciona sequelize

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-const express = require('express')
+const express = require('express');
+const connection = require('./src/database');
 
 const app = express()
 
-app.use(express.json())
+app.use(express.json()) //obrigatório
+
+const tarefas = []
+
+connection.authenticate()
+console.log('Connection has been established successfully.');
 
 app.get('/', (request, response) => {
     console.log("Entrei aqui")
-    response.json({messagem: "Bem vindo"})
+    response.json({ messagem: "Bem vindo" })
 })
 
+// Cadastrar um nova tarefa
 app.post('/tarefas', (request, response) => {
-    console.log(request.body)
-    response.json({messagem: 'tudo bem'})
+    const tarefa = {
+        nome: request.body.nome,
+        descricao: request.body.descricao
+    }
+
+    tarefas.push(request.body.tarefas)
+
+    response.status(201).json(tarefa) // recomendada
+    // response.json(tarefa, 201) antiga 
 })
+
+app.get('/tarefas', (request, response) => {
+    response.json(tarefas)
+})
+
+
 
 app.listen(3333, () => console.log("Aplicação online"))
 
